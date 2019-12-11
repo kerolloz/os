1304a955abf6e1bb6dcfb78f8fc0caa23aa06685
Kerollos <kerolloz@yahoo.com>
2019-12-11 23:06:06 +0200

    enhance code and output

--- shell.cc
+++ shell.cc
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-void welcome() {
+void hello() {
   printf(" _   _ _____ _     _     ___   \n"
          "| | | | ____| |   | |   / _ \\ \n"
          "| |_| |  _| | |   | |  | | | | \n"
@@ -18,60 +18,83 @@ void welcome() {
          "|_| |_|_____|_____|_____\\___/ \n");
 }
 
+void bye() {
+  printf("\n"
+         " ______   _______ \n"
+         "| __ ) \\ / / ____|\n"
+         "|  _ \\\\ V /|  _|  \n"
+         "| |_) || | | |___ \n"
+         "|____/ |_| |_____|\n");
+}
+
 bool isChildProcess(pid_t pid) { return pid == 0; }
 
 struct command {
-  string s;
+  string command_string;
   vector<char *> argschar;
-  bool hasPipe;
-  command(string s) : s(s) {
+  command(string s) : command_string(s) {
     vector<string> args;
     split(args);
     for (int i = 0; i < args.size(); i++) {
       char *arg = const_cast<char *>(args[i].c_str());
-      cout << arg << endl;
       argschar.push_back(arg);
     }
   }
   void split(vector<string> &commands) {
-    hasPipe = true;
-    string str = "";
-    for (int i = 0; i < s.size(); i++) {
-      if (isspace(s[i])) {
-        if (str.size())
-          commands.push_back(str);
-        str = "";
+    // just like python split
+    // intput(string): "ls -la --color"
+    // output(array of strings): ["ls", "-la", "--color"]
+    string tmp_str = "";
+    for (char c : command_string) {
+      if (isspace(c)) {
+        if (tmp_str.size())
+          commands.push_back(tmp_str);
+        tmp_str = "";
       } else
-        str += s[i];
+        tmp_str += c;
     }
-    if (str.size())
-      commands.push_back(str);
+    if (tmp_str.size())
+      commands.push_back(tmp_str);
   }
   const char *get_command() { return argschar[0]; }
   char *const *get_args() { return argschar.data(); }
-  void execute() { execvp(get_command(), get_args()); }
+  void execute() {
+    /*
+    we use execvp because it takes an array as the second argument
+    this will ease sending arguments
+    execvp searches in the `PATH' environment variable if the first argument
+    contains no slashes
+    */
+    execvp(get_command(), get_args());
+  }
 };
 
 int main() {
-  welcome();
+  hello();
   string user_input;
   while (1) {
-    printf("\n> ");
+    printf("\n[shell@user]$ ");
 
     getline(cin, user_input);
     if (cin.eof() || user_input == "exit") {
-      // if pressed CTRL + D "EOF" or entered exit
+      // if pressed CTRL + D "EOF" or entered "exit"
       break;
     }
     pid_t process_id = fork();
+    if (process_id < 0) {
+      printf("Failed to fork");
+      exit(-1);
+    }
     if (isChildProcess(process_id)) {
       command cmd(user_input);
+      // (execute) replaces the child process image with the command to execute
       cmd.execute();
-      printf("ERROR\n");
-      return 0; // return zero to stop the child process from continue if failed
+      // the following lines will execute, IFF command failed to execute
+      printf("ERROR executing: %s\n", user_input.c_str());
+      exit(0); // stop the child process, if failed
     }
-    wait(NULL);
+    wait(NULL); // wait for the child to finish execution
   }
-  printf("\nOkay, Bye!\n");
+  bye();
   return 0;
 }
